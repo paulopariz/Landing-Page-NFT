4faee8fbb5ef5dba5972c71b21860a76c2895891
Paulo Pariz <paulopariz@MacBook-Pro-de-Paulo.local>
2022-12-21 01:55:59 -0300

    config initial

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/images/logo.png">
+  <MayNav msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MayNav from './components/Nav/MyNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MayNav
   }
 }
 </script>
