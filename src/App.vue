28e881fce1211ce4e001c3fe9a1762e700723cdb
Paulo Pariz <paulopariz@MacBook-Pro-de-Paulo.local>
2022-12-22 02:13:26 -0300

    slide

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,21 @@
 
     <MyHeader />
 
+
+    <div class="gallery">
+      <h1>Super Hot Drop</h1>
+
+      <MySlide />
+      
+    </div>
+    
   </div>
 </template>
 
 <script>
 import MyNav from './components/Nav/MyNav.vue'
 import MyHeader from './components/Header/MyHeader.vue'
+import MySlide from './components/Slide/MySlide.vue'
 
 
 export default {
@@ -19,18 +28,18 @@ export default {
   components: {
     MyNav,
     MyHeader,
-
+    MySlide
   }
 }
 </script>
 
 
 
-<!--RESET-->
-<style>
+
+<style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@200;400;500;600;700;800&display=swap');
 
-
+/*RESET*/
   *{
     margin: 0;
     padding: 0;
@@ -58,6 +67,19 @@ img{
 
 body{
   background: #110929;
+}
+
+
+/*CONTENT*/
+
+.gallery{
+  margin-top: 9.0625rem;
+  h1{
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 58px;
+    letter-spacing: -0.02em;
+  }
 }
 
 </style>
---
+++ src/components/Slide/MySlide.vue
@@ -0,0 +1,79 @@
+<template>
+
+<div class="desktop">
+  <button>Hope Ape</button>
+  <button>Abstract</button>
+  <button>Mongkey</button>
+  <button>Cars</button>
+  <button>Art</button>
+</div>
+
+<div class="mobile">
+  <swiper :slidesPerView="2.5"
+          :freeMode="true"
+          :pagination="{clickable: false,}"
+          :modules="modules"
+          class="mySwiper">
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide>
+      <swiper-slide>Slide 5</swiper-slide>
+  </swiper>
+</div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide} from "swiper/vue";
+
+import "swiper/css";
+
+import "swiper/css/free-mode";
+
+import {
+    FreeMode
+} from "swiper";
+
+export default {
+    setup() {
+        return {
+            modules: [FreeMode],
+        };
+    },
+
+    components: {
+        Swiper,
+        SwiperSlide,
+
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/_buttons.scss";
+
+.swiper-slide{
+  background-color: red;
+  width: 100px !important;
+}
+.mobile{
+  display: none;
+}
+
+.desktop{
+  margin-top: 35px;
+  display: flex;
+  gap: 22px;
+
+  button{
+    @include buttons;
+    background: #39324D;
+    &:focus{
+      background: linear-gradient(112.62deg, #8D1CFE 29.4%, #0038ED 105.15%);
+    }
+  }
+}
+
+  
+
+</style>
